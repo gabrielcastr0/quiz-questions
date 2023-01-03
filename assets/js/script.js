1a2d9c433fc5cc51cd2f459a569978383a011c39
Gabriel Castro <gabrielcastro.xy@gmail.com>
2023-01-03 11:41:55 -0300

    feat: Creating function to show question

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Initial data
+let currentQuestion = 0;
+
+showQuestion();
+
+// Functions
+function showQuestion(){
+  if(questions[currentQuestion]){
+    let q = questions[currentQuestion];
+
+    document.querySelector('.scoreArea').style.display = 'none';
+    document.querySelector('.questionArea').style.display = 'block';
+
+    document.querySelector('.question').innerHTML = q.question;
+
+    let optionsHtml = '';
+
+    for(let i in q.options){
+      optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`
+    }
+
+    document.querySelector('.options').innerHTML = optionsHtml;
+  }else{
+
+ }
+}
