e1f519136e09a140e5d595b1159cd09d61fdb6a8
Gabriel Castro <gabrielcastro.xy@gmail.com>
2023-01-06 17:59:42 -0300

    feat: Refactoring somethings and change styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,84 @@
-// Initial data
+const scoreAreaButton = document.querySelector('.score-area button');
+scoreAreaButton.addEventListener('click', resetEvent);
+
+const progressBar = document.querySelector('.progress--bar');
+const scoreArea = document.querySelector('.score-area');
+const questionArea = document.querySelector('.question-section');
+const question = document.querySelector('.question');
+const options = document.querySelector('.options');
+const messageFeedback = document.querySelector('.message--feedback');
+
 let currentQuestion = 0;
 let correctAnswers = 0;
+let optionSelected = false;
 
 showQuestion();
 
-// Events 
-document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
-
-// Functions
-function showQuestion(){
-  if(questions[currentQuestion]){
-    let q = questions[currentQuestion];
-
-    // number of questions percentage
-    let percentage = Math.floor((currentQuestion / questions.length) * 100);
-
-    // changing progress bar width according percentage value
-    document.querySelector('.progress--bar').style.width = `${percentage}%`;
-
-    document.querySelector('.scoreArea').style.display = 'none';
-    document.querySelector('.questionArea').style.display = 'block';
-
-    document.querySelector('.question').innerHTML = q.question;
-
-    let optionsHtml = '';
-
-    for(let i in q.options){
-      optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`
-    }
-
-    document.querySelector('.options').innerHTML = optionsHtml;
-
-    document.querySelectorAll('.options .option').forEach(item => {
-      item.addEventListener('click', optionClickEvent);
+function showQuestion() {
+  if (questions[currentQuestion]) {
+    const q = questions[currentQuestion];
+    const percentage = Math.floor((currentQuestion / questions.length) * 100);
+    progressBar.style.width = `${percentage}%`;
+    scoreArea.style.display = 'none';
+    questionArea.style.display = 'flex';
+    question.innerHTML = q.question;
+    options.innerHTML = q.options.map((option, index) => {
+      return `<div data-op="${index}" class="option"><span>${index + 1}</span> ${option}</div>`;
+    }).join('');
+    document.querySelectorAll('.options .option').forEach(button => {
+      button.addEventListener('click', handleOptionClick);
     });
-  }else{
+  } else {
     finishQuiz();
- }
+  }
 }
 
-function optionClickEvent(e){
-  let clickedOption = parseInt(e.target.getAttribute('data-op'));
+function handleOptionClick(event) {
+  if (optionSelected) {
+    return;
+  }
 
-  if(questions[currentQuestion].answer === clickedOption){
+  optionSelected = true;
+
+  const clickedOption = parseInt(event.target.getAttribute('data-op'));
+  if (questions[currentQuestion].answer === clickedOption) {
+    messageFeedback.style.color = '#fff';
+    messageFeedback.innerHTML = 'Resposta correta';
     correctAnswers++;
+  } else {
+    messageFeedback.style.color = '#fff';
+    messageFeedback.innerHTML = 'Resposta incorreta';
   }
-
-  currentQuestion++;
-  showQuestion();
+  
+  setTimeout(() => {
+    messageFeedback.innerHTML = '';
+    currentQuestion++;
+    showQuestion();
+    optionSelected = false;
+  }, 2000);
 }
 
-function finishQuiz(){
-  let points = Math.floor((correctAnswers / questions.length) * 100);
+function finishQuiz() {
+  const points = Math.floor((correctAnswers / questions.length) * 100);
+  const scorePct = document.querySelector('.score-pct');
+  const scoreText2 = document.querySelector('.score-text-2');
+  scorePct.innerHTML = `Acertou ${points}%`;
+  scoreText2.innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}`;
 
-  if(points < 30){
-    document.querySelector('.scorePct').style.color = '#FF0000';
-  }else if(points >= 30 && points < 70){
-    document.querySelector('.scorePct').style.color = '#FFF000';
-  }else if(points >= 70){
-    document.querySelector('.scorePct').style.color = '#00FF00';
+  if (points < 30) {
+    scorePct.style.color = '#FF0000';
+  } else if (points >= 30 && points < 70) {
+    scorePct.style.color = '#FFF000';
+  } else if (points >= 70) {
+    scorePct.style.color = '#00FF00';
   }
 
-  document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
-  document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswers}`;
-
-  document.querySelector('.scoreArea').style.display = 'block';
-  document.querySelector('.questionArea').style.display = 'none';
-  document.querySelector('.progress--bar').style.width = `100%`;
+  scoreArea.style.display = 'block';
+  questionArea.style.display = 'none';
+  progressBar.style.width = `100%`;
 }
 
-function resetEvent(){
+function resetEvent() {
   correctAnswers = 0;
   currentQuestion = 0;
   showQuestion();
--- questions.js
+++ questions.js
@@ -1,95 +1,53 @@
-let questions = [
-    {
-        question: 'Em qual elemento HTML nós colocamos o javascript?',
-        options: [
-            'tag js',
-            'tag script',
-            'tag scripting',
-            'tag javascript'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Onde é o local certo de colocar o javascript?',
-        options: [
-            'Na tag body',
-            'Na tag head',
-            'Tanto na tag body quanto a head estão corretas'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Qual a sintaxe correta para adicionar um arquivo javascript externo na tag script?',
-        options: [
-            'name="xxx.js"',
-            'src="xxx.js"',
-            'href="xxx.js"'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Um arquivo javascript externo precisa ter a tag script',
-        options: [
-            'Verdade',
-            'Falso'
-        ],
-        answer: 1
-    },
-    {
-        question: 'Como escrever um "Olá Mundo" em um alertbox?',
-        options: [
-            'alertBox("Olá Mundo")',
-            'msgBox("Olá Mundo")',
-            'msg("Olá Mundo")',
-            'alert("Olá Mundo")'
-        ],
-        answer: 3
-    },
-    {
-        question: 'Como você cria uma função no Javascript?',
-        options: [
-            'function:minhaFuncao()',
-            'function = minhaFuncao()',
-            'function minhaFuncao()'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Como chamar uma função chamada "minhaFuncao"?',
-        options: [
-            'call minhaFuncao',
-            'call function minhaFuncao',
-            'minhaFuncao()'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Como escrever uma condicional IF no Javascript?',
-        options: [
-            'if i = 5',
-            'if i == 5 then',
-            'if (i == 5)',
-            'if i = 5 then'
-        ],
-        answer: 2
-    },
-    {
-        question: 'Como fazer um if que executa um código caso "i" for diferente de 5',
-        options: [
-            'if (i != 5)',
-            'if (i <> 5)',
-            'if i <> 5',
-            'if i =! 5 then'
-        ],
-        answer: 0
-    },
-    {
-        question: 'Como o loop while começa?',
-        options: [
-            'while (i <= 10)',
-            'while (i <= 10; i++)',
-            'while i = 1 to 10'
-        ],
-        answer: 0
-    },
-];
+const questions = [
+  {
+    question: 'O que é o React?',
+    options: [
+      'Uma biblioteca JavaScript para criar interfaces de usuário',
+      'Uma ferramenta para gerenciar o estado em aplicativos da web',
+      'Um framework de renderização no lado do servidor',
+      'Um gerenciador de pacotes para bibliotecas JavaScript'
+    ],
+    answer: 1
+  },
+  {
+    question: 'Como o estado é geralmente gerenciado em uma aplicação do React?',
+    options: [
+      'Usando um objeto de estado global',
+      'Usando props para passar dados entre componentes',
+      'Usando uma ferramenta de gerenciamento de estado centralizada como o Redux',
+      'Usando o estado interno do componente'
+    ],
+    answer: 3
+  },
+  {
+    question: 'O que é um componente de ordem superior no React?',
+    options: [
+      'Um componente que é renderizado no servidor',
+      'Um componente que é renderizado no cliente',
+      'Uma função que recebe um componente como argumento e retorna um novo componente',
+      'Um componente que gerencia o fetching de dados'
+    ],
+    answer: 3
+  },
+  {
+    question: 'Como você pode vincular um valor específico de this a um método de classe em um componente do React?',
+    options: [
+      'Usando o método `bind` no construtor do componente',
+      'Usando o método `call` no método',
+      'Usando o método `apply` no método',
+      'Usando uma função de seta'
+    ],
+    answer: 0
+  },
+  {
+    question: 'Qual é o propósito do método `render` em um componente do React?',
+    options: [
+      'Definir o estado inicial do componente',
+      'Definir os tipos de propriedades do componente',
+      'Definir os manipuladores de eventos do componente',
+      'Retornar o JSX que deve ser renderizado para o componente'
+    ],
+    answer: 3
+  }
+]
+  
